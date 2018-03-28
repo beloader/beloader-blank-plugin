a735fcc1221f460511bfc07b9be57b114720435d
Liqueur de Toile <contact@liqueurdetoile.com>
2018-03-28 20:52:58 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+/**
+*  @file index.js
+*  @author Liqueur de Toile <contact@liqueurdetoile.com>
+*  @licence AGPL-3.0 {@link https://github.com/liqueurdetoile/beloader/blob/master/LICENSE}
+*/
+
+import AbstractPlugin from 'core/AbstractPlugin';
+
+export default class BlankPlugin extends AbstractPlugin {
+  init(options) {
+
+  }
+}
+
+export {BlankPlugin};
